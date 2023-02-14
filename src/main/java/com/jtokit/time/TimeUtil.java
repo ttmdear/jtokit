55549e2b724070dc74fce3cbc830949cc58aa0fa
Paweł Bobryk <bobryk.pawel@gmail.com>
2023-02-14 15:57:18 +0100

    sync

--- src/main/java/com/jtokit/time/TimeUtil.java
+++ src/main/java/com/jtokit/time/TimeUtil.java
@@ -1,7 +1,29 @@
 package com.jtokit.time;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class TimeUtil {
     public static LocalDateTime MAX_DATE_TIME = LocalDateTime.of(9999, 12, 31, 23, 59);
+    private static TimeProvider GLOBAL_TIME_PROVIDER = new RealTimeProvider();
+
+    public static LocalDateTime getDateTime() {
+        return requireGlobalTimeProvider().getDateTime();
+    }
+
+    public static LocalDate getDate() {
+        return requireGlobalTimeProvider().getDate();
+    }
+
+    public static synchronized void setGlobalTimeProvider(TimeProvider timeProvider) {
+        GLOBAL_TIME_PROVIDER = timeProvider;
+    }
+
+    private static TimeProvider requireGlobalTimeProvider() {
+        if (GLOBAL_TIME_PROVIDER == null) {
+            throw new IllegalStateException("There is not GLOBAL_TIME_PROVIDER set.");
+        }
+
+        return GLOBAL_TIME_PROVIDER;
+    }
 }
